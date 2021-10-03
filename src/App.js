1a5f41d27e29c9c09b46c5c9c8ee190097040d89
sshankar02 <shankar.saurabh08@gmail.com>
2021-10-03 21:34:22 +0530

    Setup up home page.
    Home page setup. deleted un necessary test files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
 import './App.css';
 // Components
-import NewsCollection from './molecules/NewsCollection';
+import NewsHome from './organism/NewsHome';
+import NewsDetails from './organism/NewsDetails';
 
 // Services
 import GetTopHeadlines from "./services/GetTopHeadlines";
@@ -9,16 +10,18 @@ import GetTopHeadlines from "./services/GetTopHeadlines";
 function App() {
 
   const [newsItems, setNewsItems] = useState([]);
+  const [openedNewsItem, setOpenedNewsItem] = useState('');
 
   useEffect(()=>{
     GetTopHeadlines('mock')
       .then(response => setNewsItems(response.data.articles));
   },[])
 
+
   return (
     <div className="App">
       <h1>News App</h1>
-      {<NewsCollection data={newsItems}/>}
+      {openedNewsItem ? <NewsDetails /> : <NewsHome rows={newsItems} />}
     </div>
   );
 }
--- src/organism/NewsHome/NewsHome.js
+++ src/organism/NewsHome/NewsHome.js
@@ -1,7 +1,9 @@
+import NewsCollection from "../../molecules/NewsCollection";
 import './NewsHome.css'
 
-const NewsHome = () =>{
-    return 'News Home';
+const NewsHome = props =>{
+    const {rows=[]} = props;
+    return <div className="news-home"><div className="news-collection-container"><NewsCollection rows={rows}/></div></div>
 }
 
 export default NewsHome;
